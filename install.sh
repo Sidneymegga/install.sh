c36afa05e8024438c30b7992251a30bb665d4782
Sidneymegga <147114025+Sidneymegga@users.noreply.github.com>
2024-03-01 03:42:12 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,44 +6,51 @@ sudo apt-get install -y python3-pip
 sudo pip3 install psutil
 
 # Criação do script de monitoramento
-echo "
+echo '
 import psutil
 import os
-import socket
 import datetime
+import subprocess
 
-host = '127.0.0.1'
-ports = [80, 8080]
-cpu_threshold = 70
-
-def check_port(port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)
-    result = sock.connect_ex((host, port))
-    sock.close()
-    return result == 0
+portas = [80, 8080]
+cpu_threshold = 80
 
 def restart_proxy():
-    for port in ports:
-        os.system(f'sudo service proxy-{port} restart')
-    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    log_entry = f'{current_time} - MEGGA CLOUD REINICIANDO PROXY.'
-    with open('/root/logfile.txt', 'a') as logfile:
-        logfile.write(log_entry + '\n')
+    for port in portas:
+        os.system(f"sudo service proxy-{port} restart")
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_entry = f"{current_time} - MEGGA CLOUD REINICIANDO PROXY."
+    with open("/root/logfile.txt", "a") as logfile:
+        logfile.write(log_entry + "\n")
+    print(log_entry)
+
+def restart_journald():
+    subprocess.run(["sudo", "systemctl", "restart", "systemd-journald"])
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_entry = f"{current_time} - REINICIANDO JOURNALD DEVIDO AO ALTO USO DE CPU."
+    with open("/root/logfile.txt", "a") as logfile:
+        logfile.write(log_entry + "\n")
+    print(log_entry)
 
 def main():
+    # Monitora o uso de CPU
     cpu_usage = psutil.cpu_percent(interval=1)
+    print(f"CPU Usage: {cpu_usage}")
     if cpu_usage > cpu_threshold:
         restart_proxy()
-    else:
-        for port in ports:
-            if not check_port(port):
-                restart_proxy()
 
-if __name__ == '__main__':
+    # Monitora o processo systemd-journald
+    for proc in psutil.process_iter(["pid", "name", "cpu_percent"]):
+        if proc.info["name"] == "systemd-journald":
+            journald_cpu_usage = proc.info["cpu_percent"]
+            print(f"Journald CPU Usage: {journald_cpu_usage}")
+            if journald_cpu_usage > cpu_threshold:
+                restart_journald()
+                break
+
+if __name__ == "__main__":
     main()
-
-" > /root/monitor_cpu.py
+' > /root/monitor_cpu.py
 
 # Permissões necessárias
 sudo chmod +x /root/monitor_cpu.py
