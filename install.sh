c2db1f4b262e71725491bc9b80d6873c54263bdd
Sidneymegga <147114025+Sidneymegga@users.noreply.github.com>
2024-03-02 01:35:46 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ import subprocess
 import socket
 import time
 
-host = '127.0.0.1'
+host = "127.0.0.1"
 ports = [80, 8080]
 cpu_threshold = 80
 verification_count = 0
@@ -28,23 +28,23 @@ def check_port(port):
 
 def restart_proxy():
     for port in ports:
-        os.system(f'sudo service proxy-{port} restart')
-    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    log_entry = f'{current_time} - MEGGA CLOUD REINICIANDO PROXY.'
-    with open('/root/logfile.txt', 'a') as logfile:
-        logfile.write(log_entry + '\n')
+        os.system(f"sudo service proxy-{port} restart")
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_entry = f"{current_time} - MEGGA CLOUD REINICIANDO PROXY."
+    with open("/root/logfile.txt", "a") as logfile:
+        logfile.write(log_entry + "\n")
     print(log_entry)
 
 def main():
     global verification_count
 
     cpu_usage = psutil.cpu_percent()
-    print(f'CPU Usage: {cpu_usage}')  # Mostra o uso da CPU ao iniciar o script
+    print(f"CPU Usage: {cpu_usage}")  # Mostra o uso da CPU ao iniciar o script
 
     # Loop para fazer 3 verificações a cada 10 minutos
     for i in range(3):
         cpu_usage = psutil.cpu_percent(interval=1)
-        print(f'CPU Usage: {cpu_usage}')
+        print(f"CPU Usage: {cpu_usage}")
 
         if cpu_usage > cpu_threshold:
             verification_count += 1
@@ -56,16 +56,14 @@ def main():
     if verification_count == 3:
         restart_proxy()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 ' > /root/monitor_cpu.py
 
-# Permissões necessárias
+# Permissões necessárias para o script de monitoramento
 sudo chmod +x /root/monitor_cpu.py
-sudo touch /root/logfile.txt
-sudo chmod 644 /root/logfile.txt
 
-# Adiciona comando de inicialização no cron para executar a cada 10 minutos
+# Configuração do cron para executar o monitoramento a cada 10 minutos
 (crontab -l ; echo "*/10 * * * * /usr/bin/python3 /root/monitor_cpu.py > /dev/null 2>&1") | crontab -
 
 echo "Instalação concluída com sucesso!"
